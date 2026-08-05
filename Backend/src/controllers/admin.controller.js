a889be5ad3668c8be330ec3d27c446bf555f4c42
Aditya Ranjan Patra <aditya.dbiit2@gmail.com>
2026-08-05 12:02:48 +0530

    add some changes in admin to codebase

--- Backend/src/controllers/admin.controller.js
+++ Backend/src/controllers/admin.controller.js
@@ -25,39 +25,10 @@ const adminRegister = async (req, res) => {
   }
 };
 
-const adminLogin = async (req, res) => {
-  try {
-    const { emailId, password } = req.body;
-    if (!emailId) {
-      throw new Error("Invalid Credentials");
-    }
-    if (!password) throw new Error("Invalid Credentials");
 
-    const user = await User.findOne({ emailId });
-    if(!user || user.role !== "admin") {
-      throw new Error("Invalid Credentials");
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      throw new Error("Invalid Credentials");
-    }
-    const token = jwt.sign(
-      { _id: user._id, emailId: emailId, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" },
-    );
-    res.cookie("token", token, { maxAge: 60 * 60 * 1000 });
-    res.status(200).send(`${user.role} logged in successfully`);
-  } catch (err) {
-    res.status(400).send("Error: " + err.message);
-  }
-};
 
 
 
 module.exports = {
-  adminRegister,
-  adminLogin,
+  adminRegister
 };
--- Backend/src/routes/admin.route.js
+++ Backend/src/routes/admin.route.js
@@ -1,14 +1,12 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin.middleware");
-const { adminRegister,  adminLogin } = require("../controllers/admin.controller");
+const { adminRegister } = require("../controllers/admin.controller");
 const { logout } = require("../controllers/user.controller");
 
 
 const adminRouter=Router();
 
 adminRouter.post("/register", adminMiddleware, adminRegister);
-adminRouter.post("/login", adminLogin);
-adminRouter.post("/logout", adminMiddleware, logout);
 
 
 module.exports=adminRouter;
